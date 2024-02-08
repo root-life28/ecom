8a4bb6813095ed9ba4ffbfffe1044714250368b7
muzahidul <bekon28@outlook.com>
2024-02-08 15:25:59 +0600

    cn

---
+++ backend/config/db.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const connectDB=async ()=>{
+    try {
+        const conn=await mongoose.connect(process.env.MONGO_URI);
+
+        console.log(`MongoDb connect ${conn.connection.host}`);
+       
+    } catch (error) {
+        console.log(`Error ${error}`);
+        process.exit(1);
+        
+    }
+}
+
+export default connectDB;
--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,21 @@
 import  express  from "express";
-import products from './data/products.js';
+import productRoutes from '../backend/routes/productRoutes.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import dotenv from 'dotenv'
 dotenv.config()
 
-const port=process.env.PORT;
+import connectDB from "./config/db.js";
 
+const port=process.env.PORT;
+connectDB();
 const app=express();
 
 app.get('/',(req,res)=>{
     res.send('Api is running...');
 })
 
-
-app.get('/api/products',(req,res)=>{
-    res.json(products);
-})
-app.get('/api/products/:id',(req,res)=>{
-
-    const product=products.find((p)=>p._id==req.params.id);
-    res.json(product);
-})
-
-
-
-
+app.use('/api/products',productRoutes);
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port,()=>console.log(`server running on port ${port}`));
